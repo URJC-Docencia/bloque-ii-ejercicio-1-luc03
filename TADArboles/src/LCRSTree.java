d18774e64614ca258ccfde04f0a4118d712b3485
luc03 <l.sangregorio.2021@alumnos.urcj.es>
2023-10-03 19:55:33 +0200

    TreeLCRS

--- TADArboles/src/LCRSTree.java
+++ TADArboles/src/LCRSTree.java
@@ -9,8 +9,35 @@ import java.util.Iterator;
  * @param <E> the type of elements stored in the tree
  */
 public class LCRSTree<E> implements NAryTree<E> {
-
+    private int size;
+    private  LCRSnode<E> root;
     private class LCRSnode<T> implements Position<T> {
+        private E element;
+        private LCRSnode<E> parent;
+        private LCRSnode<E> child;
+        private LCRSnode<E> siblling;
+
+        public LCRSnode(E element, LCRSnode<E> parent) {
+            this.element = element;
+            this.parent = parent;
+        }
+
+        public LCRSnode(E element) {
+            this.element = element;
+        }
+
+        public LCRSnode<E> getParent() {
+            return parent;
+        }
+
+        public LCRSnode<E> getChild() {
+            return child;
+        }
+
+        public LCRSnode<E> getSiblling() {
+            return siblling;
+
+        }
 
         @Override
         public T getElement() {
@@ -21,7 +48,12 @@ public class LCRSTree<E> implements NAryTree<E> {
 
     @Override
     public Position<E> addRoot(E e) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(!isEmpty()){
+            throw new RuntimeException("The tree has already a root node");
+        }
+        root = new LCRSnode<E>(e);
+        size++;
+        return root;
     }
 
     @Override
@@ -61,12 +93,14 @@ public class LCRSTree<E> implements NAryTree<E> {
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        return size == 0;
     }
 
     @Override
     public Position<E> root() {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        return root;
     }
 
     @Override
